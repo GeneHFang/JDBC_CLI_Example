8aaafc7311a8a8daea3788cfc392e04e172b2387
GeneHFang <gene.li@nyu.edu>
2020-06-17 19:18:15 -0400

    java DBMS example

---
+++ ExampleQuery.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+public class ExampleQuery {
+
+    //Command line example 
+    //IN CLI: java ExampleQuery <url> <user> <pw> <driver> <SQL query as string>
+    public static void main(String[] args) {
+        
+        ResultSet rs = null;
+        Statement stmt = null;
+        Connection conn = null;
+
+        String _url = args[0];
+
+        try {
+            //Step 1) Load driver 
+            Class.forName(args[3]);
+
+            //Step 2) make connection 
+            conn = DriverManager.getConnection(_url, args[1], args[2]);
+
+            //Step 3) create statement
+            stmt = conn.createStatement();
+
+            //Step 4) Make query
+            rs = stmt.executeQuery(args[4]);
+           
+            //Step 5) Display results
+            while (rs.next()){
+                System.out.print(rs.getString(1));
+                //and other sql getters : getInt(), getDate()
+                System.out.println(rs.getDate(4));
+            }  
+        }
+        //handle any exceptions
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        //clean up resources
+        finally{
+            try {
+                if (rs!=null) rs.close();
+                if (stmt!=null) stmt.close();
+                if (conn!=null) conn.close();
+            }
+            catch (SQLException se){
+                se.printStackTrace();
+            }
+        }
+
+    }
+
+}
